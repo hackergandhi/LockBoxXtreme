3fd7dd5ec5b913f13605b7cf2e004f217d7c7ae6
Ved <vedthorat1029@gmail.com>
2024-11-16 22:11:16 +0530

    added utils.py

---
+++ main/utils.py
@@ -0,0 +1,11 @@
+def store_password(service, password, master_password):
+    pass
+
+def retrieve_password(service, master_password):
+    pass
+
+def delete_service(service):
+    pass
+
+def backup_passwords(master_password):
+    pass
